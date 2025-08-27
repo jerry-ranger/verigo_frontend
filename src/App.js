06e0805f41f388748a7010b59eeb7166f0fedc5d
Jerry J <jerry@Jerrys-MacBook-Air.local>
2025-08-27 17:41:03 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { Container, Paper, TextField, Button, Typography, Box } from '@mui/material';
+import { useState } from 'react';
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('Login:', { email, password });
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8 }}>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleLogin}>
+            <TextField
+              fullWidth
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              margin="normal"
+              required
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              margin="normal"
+              required
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3 }}
+            >
+              Login
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
